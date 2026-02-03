ea7abfe8710b860eaf2a19b231dd374a916c1596
feuerdoge <deniboz11@gmail.com>
2026-02-03 14:42:38 +0100

    Structure

---
+++ socialMediaServer/Server.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SocketAbi;
+
+namespace socialMediaServer
+{
+    public class Server
+    {
+        ServerSocket socket;
+
+        public Server(int port) 
+        {
+            this.socket = new ServerSocket(port);
+        }
+
+        public void runServer() 
+        {
+            while(true) 
+            {
+                socket.Accept();
+                ServerThread thread = new ServerThread();
+            }
+            
+        } 
+    }
+}
