29470e2dad11da8eeb3834d23d2a55e18dfdb43e
trupper61 <rupert.hoffmann.06@gmail.com>
2026-02-20 10:29:50 +0100

    fix

--- socialMediaServer/Server.cs
+++ socialMediaServer/Server.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Permissions;
 using System.Text;
@@ -17,6 +18,8 @@ namespace socialMediaServer
         {
             this.Serversocket = new ServerSocket(port);
             spf = new SocialMediaPlatform();
+            if (!Directory.Exists("img"))
+                Directory.CreateDirectory("img");
             runServer();
         }
 
